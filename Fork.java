55165f948913bf1a202da64540fec6be48ea658c
Erik Hynes <erikhynes@gmail.com>
2021-04-24 14:12:25 -0400

    hi again!

---
+++ Fork.java
@@ -0,0 +1,8 @@
+public class Fork {
+
+    public boolean isHeld;
+
+    public Fork(){
+        isHeld = false;
+    }
+}
---
+++ Simulation.java
@@ -0,0 +1,22 @@
+public class Simulation {
+
+
+    Philosopher[] phils = new Philosopher[5];
+    Fork[] forks = new Fork[5];
+
+    public static void main(String[] args){
+
+        for(int i = 0; i < 5; i++){
+            phils[i].
+        }
+
+        /* Each philosopher has a left and right fork
+            Adjacent philosophers share a fork
+            The fork is either held or not held (by one of the philosophers)
+            A philosopher can pick up the fork if it isn't held
+            When a phil wants to eat, they need to hold both forks 
+                -maybe phil has boolean for holdingLeft and holdingRight
+            
+        */
+    }
+}
