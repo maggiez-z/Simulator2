a881811ae1513489e206a54705944c02d77f7a9f
Erik Hynes <erikhynes@gmail.com>
2021-04-24 15:18:25 -0400

    hi

--- Philosopher.java
+++ Philosopher.java
@@ -2,8 +2,8 @@ public class Philosopher implements Runnable{
 
     private Fork forkLeft;
     private Fork forkRight;
-    public boolean isUsingLeft;
-    public boolean isUsingRight;
+    public boolean isHoldingLeft;
+    public boolean isHoldingRight;
     
     
     @Override
@@ -28,8 +28,38 @@ public class Philosopher implements Runnable{
         }
     }
 
-    public void eat() {
+    public void takeLeftFork(){
+        forkLeft.isHeld = true;
+        isHoldingLeft = true;
+    }
+    public void takeRightFork(){
+        forkRight.isHeld = true;
+        isHoldingRight = true;
+    }
+    public void dropLeftFork(){
+        forkLeft.isHeld = false;
+        isHoldingLeft = false;
+    }
+    public void dropRightFork(){
+        forkRight.isHeld = false;
+        isHoldingRight = false;
+    }
 
+    public void dine() {
+        while(true){
+            //Basic scenario where you take the fork if you can (doesnt avoid deadlocks)
+            if(!forkLeft.isHeld)
+                takeLeftFork();
+            if(!forkRight.isHeld)
+                takeRightFork();
+            //Eat if you have both forks
+            if(isHoldingLeft && isHoldingRight) { 
+                //Eat for 1 second
+                Thread.sleep(1000); 
+                dropLeftFork();
+                dropRightFork();
+            }
+        }
     
     }
 }
